135e2dc704e3aa3765f031b575f2a9b701b24071
bbbddd84 <fibruck@gymcheb.cz>
2026-03-26 12:09:01 +0100

    Update main.py

--- Lekce_03_Datové typy/main.py
+++ Lekce_03_Datové typy/main.py
@@ -1,18 +1,33 @@
 # ÚLOHA 1: Sčítací past (Cíl: 5 + 3 = 8)
 # ---------------------------------------------------------
+a=int(input("Zadej první číslo: "))
+b=int(input("Zadej druhé číslo: "))
+print("Součet je:", a + b)
 
 
 # ÚLOHA 2: Opakovač jména (Cíl: "Honza" a 3 -> "HonzaHonzaHonza")
 # ---------------------------------------------------------
+jmeno=input("Zadej jméno: ")
+pocet=int(input("Kolikrát ho vypsat: "))
+print(jmeno * pocet)
 
 
 # ÚLOHA 3: Rok narození (Cíl: 2026 - věk)
 # ---------------------------------------------------------
+vek=int(input("Kolik ti je let: "))
+rok=2026 - vek
+print("Narodil ses přibližně v roce", rok)
 
 
 # ÚLOHA 4: Výplata (Cíl: mzda * hodiny)
 # ---------------------------------------------------------
+mzda=int(input("Zadej hodinovou mzdu: "))
+hodiny=int(input("Zadej počet hodin: "))
+print("Celková výplata je:", mzda * hodiny)
 
 
 # ÚLOHA 5: Magická matematika (Cíl: (x + 10) * 2)
 # ---------------------------------------------------------
+x=int(input("Zadej číslo: "))
+vysledek=(x + 10) * 2
+print("Výsledek je:", vysledek)
